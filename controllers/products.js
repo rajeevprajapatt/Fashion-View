cad475760ed23893feb6d2d451b06ddd2b95c3be
rajeevprajapatt <rajeevprajapat06@gmail.com>
2024-07-30 23:27:01 +0530

    Style updates

--- controllers/products.js
+++ controllers/products.js
@@ -1,56 +1,62 @@
 const express = require("express");
-const { womenProducts, menProducts } = require("../models/productSchema");
+const { womenProducts, menProducts, womenCategories, menCategories } = require("../models/productSchema");
 
 async function GetWomenProducts(req, res) {
-    const items = await womenProducts.find({ });
+    const items = await womenProducts.find({});
+    const category = await womenCategories.findOne({ Categories: { $type: "array" } });
+    const Categories = category.Categories;
     res.render("shop", {
         data: items,
-    }) 
+        Categories
+    })
 }
 
 async function GetMenProducts(req, res) {
-    const items = await menProducts.find({ });
+    const items = await menProducts.find({});
+    const category = await menCategories.findOne({ Categories: { $type: "array" } });
+    const Categories = category.Categories;
     res.render("shop", {
         data: items,
-    }) 
+        Categories
+    })
 }
 
-async function ItemInsertion(req,res){
-        const body = req.body;
+async function ItemInsertion(req, res) {
+    const body = req.body;
 
-        let paths = [];
-        req.files.forEach(file => {
-            paths.push(`${file.destination.split("/").pop()}/${file.originalname}`);
+    let paths = [];
+    req.files.forEach(file => {
+        paths.push(`${file.destination.split("/").pop()}/${file.originalname}`);
+    })
+
+    let category = body.Product_Category;
+    if (category == "Women") {
+        await womenProducts.create({
+            Product_Name: body.Product_Name,
+            Product_Size_Type: body.Product_Size_Type,
+            Product_Description: body.Product_Description,
+            Product_Category: body.Product_Category,
+            Product_Type: body.Product_Type,
+            Product_Price: body.Product_Price,
+            Product_CutPrice: body.Product_CutPrice,
+            Product_ImgPaths: paths,
+            Product_Sizes: body.Product_Sizes.split(" "),
         })
-    
-        let category = body.Product_Category;
-        if (category == "Women") {
-            await womenProducts.create({
-                Product_Name: body.Product_Name,
-                Product_Size_Type: body.Product_Size_Type,
-                Product_Description: body.Product_Description,
-                Product_Category: body.Product_Category,
-                Product_Type: body.Product_Type,
-                Product_Price: body.Product_Price,
-                Product_CutPrice: body.Product_CutPrice,
-                Product_ImgPaths: paths,
-                Product_Sizes: body.Product_Sizes.split(" "),
-            })
-        }
-        else {
-            await menProducts.create({
-                Product_Name: body.Product_Name,
-                Product_Size_Type: body.Product_Size_Type,
-                Product_Description: body.Product_Description,
-                Product_Category: body.Product_Category,
-                Product_Type: body.Product_Type,
-                Product_Price: body.Product_Price,
-                Product_CutPrice: body.Product_CutPrice,
-                Product_ImgPaths: paths,
-                Product_Sizes: body.Product_Sizes.split(" "),
-            })
-        }
-        res.redirect("/ItemsInsertion");
+    }
+    else {
+        await menProducts.create({
+            Product_Name: body.Product_Name,
+            Product_Size_Type: body.Product_Size_Type,
+            Product_Description: body.Product_Description,
+            Product_Category: body.Product_Category,
+            Product_Type: body.Product_Type,
+            Product_Price: body.Product_Price,
+            Product_CutPrice: body.Product_CutPrice,
+            Product_ImgPaths: paths,
+            Product_Sizes: body.Product_Sizes.split(" "),
+        })
+    }
+    res.redirect("/ItemsInsertion");
 }
 module.exports = {
     GetWomenProducts,
