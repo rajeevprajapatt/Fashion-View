d20d40990e329ed9086a3bcb5d6fa0fb8d8ba006
rajeevprajapatt <rajeevprajapat06@gmail.com>
2024-09-16 23:48:55 +0530

    code optimized

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -5,9 +5,10 @@ const { HomeActivities } = require("../controllers/mainrouter");
 const multer = require("multer");
 const router = express.Router();
 
+const Product_Image_Path = './public/productImages';
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, './public/productImages');
+        return cb(null, Product_Image_Path);
     },
     filename: function (req, file, cb) {
         return cb(null, file.originalname);
@@ -15,26 +16,27 @@ const storage = multer.diskStorage({
 });
 const img = multer({ storage });
 
+// Main Route - Home 
 router.get("/", HomeActivities);
 
-router.get("/Shop", (req, res) => {
-    return res.render('shop');
-});
-
+// User Routes
 router.get("/user/login", checkUserLogged);
-
 router.get("/user/signup", (req, res) => {
     res.render('signup');
 });
 
+// Shop Routes
+router.get("/Shop", (req, res) => {
+    return res.render('shop');
+});
+router.get("/Shop/Women", AllWomenProducts);
+router.get("/Shop/Men", AllMenProducts);
+router.get("/Shop/item/:itemName", GetItem);
+
+// Item Insertion ROutes
 router.get("/ItemsInsertion", (req, res) => {
     res.render('FVitemsInsertion');
 });
-
-router.get("/Shop/Women", AllWomenProducts);
-router.get("/Shop/Men", AllMenProducts);
-router.get("/Shop/item/:itemName", GetItem )
-
 router.post("/Insert", img.array('imgPath', 4), ItemInsertion);
 
 module.exports = router;
