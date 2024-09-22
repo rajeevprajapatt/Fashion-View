23eedd061b0bb5662e44620aee32a099648414a2
rajeevprajapatt <rajeevprajapat06@gmail.com>
2024-09-22 12:06:41 +0530

    MongoDB Atlas connection Successfully

--- controllers/mainrouter.js
+++ controllers/mainrouter.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { womenCategories, menCategories, womenProducts, menProducts } = require("../models/productSchema");
-// menCategoriesModel
+const { menProducts, womenProducts } = require("../models/productSchema");
+
 async function HomeActivities(req, res) {
-    const men = await menProducts.find({});
-    const women = await womenProducts.find({});
-    console.log(men);
+    const men = await menProducts.find({ Product_Category: "Men" });
+    const women = await womenProducts.find({ Product_Category: "Women" });
     return res.render('index', {
         totalMenItems: men.length,
         totalWomenItems: women.length
     });
 }
-
 
 module.exports = {
     HomeActivities,
